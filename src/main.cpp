93648dc519d8c7392d432969b41fffd314893e86
umarnurmatov <umarkhonnur12@gmail.com>
2023-10-21 21:11:04 +0300

    added exception catch

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,24 @@ int main()
     std::string expr = "4*1.25+(-2-3^5)*1.23";
     std::cout << "Expression: " << expr << std::endl;
 
-    tokenizer.tokenize(expr, tokensInfix);
-    for(auto& i : tokensInfix) std::cout << i.getStr() << ", type = " << i.getType() << ", associative = " << i.getAsc() << "\n"; 
+    try
+    {
+        tokenizer.tokenize(expr, tokensInfix);
+        for(auto& i : tokensInfix) std::cout << i.getStr() << ", type = " << i.getType() << ", associative = " << i.getAsc() << "\n";
 
-    shuntingYard(tokensInfix, tokensRPN);
+        shuntingYard(tokensInfix, tokensRPN);
+    }
+    catch(CustomException &e)
+    {
+        std::cerr << e.what() << "\n";
+        exit(-1);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        exit(-1);
+    }
+
     std::cout << "Reverse Polish Notation: ";
     for(auto& i : tokensRPN) std::cout << i.getStr() << " "; 
     std::cout << "\n";
